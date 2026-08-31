0a73beb1acc8fdd5c3623f5a0db4a85c0b6afd0e
Mitchell <mdj43@students.uwf.edu>
2026-08-31 16:15:41 -0400

    login.php fixed connection issue

--- API/login.php
+++ API/login.php
@@ -1,6 +1,6 @@
 <?php
     $inData = json_decode(file_get_contents('php://input'), true);
-    $connection = new mysqli("localhost", "test", "test", "contactmanagerdb");
+    $connection = new mysqli("localhost", "admin", "admin", "ContactManager");
 
     if ($connection->connect_error)
     {
