907cc4601cf4669c59a8e9dfdb3b2c25b9ec4891
Mitchell Johnston <mdj43@students.uwf.edu>
2026-09-03 19:31:14 -0400

    integrated .env.
    Changed the connection to use a .env file instead of writing usernames and passwords directly in the code.

--- API/addAcct.php
+++ API/addAcct.php
@@ -1,7 +1,18 @@
 <?php
-    $inputData = json_decode(file_get_contents('php://input'), true);//make json file front end gives us as useable
-    $connection = new mysqli("localhost", "admin", "admin", "ContactManager"); //connect to our database
+    /*
+     * The following lines starting with "require_once" through "$connection"
+     * should appear at the top of every PHP file. This ensures the .env is 
+     * being used and that a connection to the database is established.
+     */
 
+    require_once __DIR__ . '/../vendor/autoload.php';
+    $dotEnv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotEnv->load();
+
+    $inData = json_decode(file_get_contents('php://input'), true);
+    $connection = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'],
+                             $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+    
     if ($connection->connect_error)//if connect error
     {
         returnError($connection->connect_error);//give that error
@@ -59,8 +70,4 @@
         header('Content-type: application/json');
         echo $object;
     }
-
-
-
-
 ?>
