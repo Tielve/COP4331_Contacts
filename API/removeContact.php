1aa321fb40b66e3f7b34d8553f85b5f674231940
Mitchell Johnston <mdj43@students.uwf.edu>
2026-09-03 19:55:35 -0400

    integrated .env.
    Changed the connection to use a .env file instead of writing usernames and passwords directly in the code.

--- API/removeContact.php
+++ API/removeContact.php
@@ -1,8 +1,17 @@
 <?php
+    /*
+     * The following lines starting with "require_once" through "$connection"
+     * should appear at the top of every PHP file. This ensures the .env is 
+     * being used and that a connection to the database is established.
+     */
 
-    $inData = json_decode(file_get_contents('php://input'), true);//decode input
-    $connection = new mysqli("localhost", "admin", "admin", "ContactManager");//connect to sql database
-    
+    require_once __DIR__ . '/../vendor/autoload.php';
+    $dotEnv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotEnv->load();
+
+    $inData = json_decode(file_get_contents('php://input'), true);
+    $connection = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'],
+                             $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 
     //get our ids
     $uID = $inData["uID"];
@@ -71,8 +80,7 @@
 
     //returns the removed cID, with removed being true
     function returnSuccess($cID)
-    {   
-        
+    {       
         $returnValue =
         '{
             "cID" : "' . $cID . '",
@@ -88,9 +96,3 @@
         echo $object;
     }
 ?>
-
-
-
-
-
-
